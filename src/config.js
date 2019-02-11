6bce48615854f38f96f35689fe0223654ff52fa2
Rob <robpeterman573@gmail.com>
2019-02-10 19:29:52 -0500

    added config file for basic local/prod server setup

---
+++ src/config.js
@@ -0,0 +1,12 @@
+let API_BASE_URL; 
+if(process.env.NODE_ENV==='production') { 
+  API_BASE_URL= 'https://lit-hamlet-54666.herokuapp.com';
+} else {  
+  API_BASE_URL='http://localhost:8080';
+} 
+
+module.exports = {
+  PORT: process.env.PORT || 8080,
+  // other stuff
+  API_BASE_URL: API_BASE_URL
+}; 
