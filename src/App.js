cfaebd4a6e03d3700ef46e7606eef77263ea3d7c
Rob <robpeterman573@gmail.com>
2019-02-10 19:23:37 -0500

    added basic component layout + GET request for US state data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,80 @@
 import React, { Component } from 'react';
-import './App.css';
+import Weather from './components/Weather';
+import Status from './components/Status';
+import Clock from './components/Clock';
+import Heading from './components/Heading';
+import RegionInfo from './components/RegionInfo';
+import { connect } from 'react-redux'
+import SocialMedia from './components/SocialMedia';
+import { API_BASE_URL } from "./config";
+import {setState} from './actions'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {activeState: 0}
+    };
+
+  componentDidMount() {
+      fetch(`${API_BASE_URL}/activate/Test`)
+        .then((res) => {
+          return res.json()
+            .then((result) => {
+              this.props.dispatch(setState(result));
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        })
+
+  }
   render() {
+    console.log(this.props.fullState)
     return (
       <div>
-        main component
+
+        <div className='headerBar'>
+          <Status status={this.props.status}/>
+          <Heading headingInfo={this.props.disasterInfo}/>
+          <Clock location={this.props.location} timeZone={this.props.timeZone}/>
+        </div>
+
+        <div className='infoBar'>
+
+          <div className='row'>      
+            <RegionInfo regionalData={this.props.regionalData}/>
+            <Weather weatherInfo={this.props.weatherInfo}/>
+          </div>
+
+          <div className='row'>
+
+            <SocialMedia/>
+            <div className='component'>
+              <h2 className='component__h2'>Not sure what to put here yet</h2>
+              <div className='component__container'>
+                <ul>
+                  <li>This could be a section for tools/toolbar links</li>
+                  <li>Graphs and other visual aids maybe</li>
+                  <li>Could be a social media analysis tab</li>
+                </ul>
+              </div> 
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  fullState: state,
+  status: state.level,
+  disasterInfo: {name: state.activationName, location: state.regionalData[state.activeState].name},
+  timeZone: state.regionalData[state.activeState].timeZone,
+  location: state.regionalData[state.activeState].name,
+  weatherInfo: state.weatherInfo,
+  regionalData: state.regionalData[state.activeState]
+})
+
+export default connect(mapStateToProps)(App)
+
