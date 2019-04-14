823d074da1d03f41784fca22711218c015c516ef
Rob <robpeterman573@gmail.com>
2019-04-14 16:23:27 -0400

    changed config file to more readable format

--- src/config.js
+++ src/config.js
@@ -1,12 +1,6 @@
-let API_BASE_URL; 
-if(process.env.NODE_ENV==='production') { 
-  API_BASE_URL= 'https://ancient-refuge-55954.herokuapp.com';
-} else {  
-  API_BASE_URL='http://localhost:8080';
-}
+//   API_BASE_URL= 'https://ancient-refuge-55954.herokuapp.com';
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  // other stuff
-  API_BASE_URL: API_BASE_URL
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080'
 }; 
