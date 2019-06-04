3278cc0d205714bb0b213c07a826d53b3a8262a8
Rob Peterman <robpeterman573@gmail.com>
2019-06-03 20:31:17 -0400

    enabling new domain to access servers

--- src/config.js
+++ src/config.js
@@ -1,5 +1,11 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080',
-  STATES_DB: process.env.REACT_APP_STATES_DB || 'http://localhost:8008'
-}; 
+  API_BASE_URL:
+    process.env.REACT_APP_API_BASE_URL ||
+    "https://ancient-refuge-55954.herokuapp.com" ||
+    "http://localhost:8080",
+  STATES_DB:
+    process.env.REACT_APP_STATES_DB ||
+    "https://boiling-citadel-50408.herokuapp.com" ||
+    "http://localhost:8008"
+};
