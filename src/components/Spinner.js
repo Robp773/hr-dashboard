bdf1e9c0712c75c0c4140b3df91e64e9216b878e
Robert Peterman <robpeterman573@gmail.com>
2019-10-11 11:03:53 -0400

    Added new props to allow width and height to be customized

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 import Loader from "react-loader-spinner";
 export default class Spinner extends React.Component {
   render() {
-    return <Loader type="Bars" color={ this.props.color ? this.props.color : "#ffffff" }height="35" width="35" />;
+    return (
+      <Loader
+        type="Bars"
+        color={this.props.color ? this.props.color : "#ffffff"}
+        height={this.props.height ? this.props.height : 35}
+        width={this.props.width ? this.props.width : 35}
+      />
+    );
   }
 }
