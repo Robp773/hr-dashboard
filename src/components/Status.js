b7cc9a4537a1bebbd473ad2c4d54525ba1fe1752
Robert Peterman <robpeterman573@gmail.com>
2019-10-11 11:04:22 -0400

    Added additional CSS properties to make sure contrast between level and background is ok

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,22 +1,20 @@
-import React from 'react';
+import React from "react";
 
-export default function Status(props){  
-    
-    let statusColor;
-    if(props.status === 1){
-        statusColor = {background: 'green'};
-    }
-    else if(props.status === 2){
-        statusColor = {color: 'yellow'};
-    }
-    else if(props.status === 3){
-        statusColor = {background: 'red'};
-    }
+export default function Status(props) {
+  let statusColor;
+  if (props.status === 1) {
+    statusColor = { background: "green", color: "white" };
+  } else if (props.status === 2) {
+    statusColor = { background: "yellow" };
+  } else if (props.status === 3) {
+    statusColor = { background: "red", color: "white" };
+  }
 
-    return(
-        <div className='status' >  
-            <div className='status__alert' style={statusColor}>{props.status}</div>
-        </div>
-    )
-
+  return (
+    <div className="status">
+      <div className="status__alert" style={statusColor}>
+        {props.status}
+      </div>
+    </div>
+  );
 }
